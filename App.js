05fba01e5e031502b045bdb596d35b0eb86beb59
alanhunt1 <30370570+alanhunt1@users.noreply.github.com>
2020-01-23 15:58:36 -0500

    initial import

---
+++ App.js
@@ -0,0 +1,115 @@
+import React from "react";
+import "./App.css";
+import PostingList from "./Component/PostingList.js";
+import PostForm from "./Component/PostForm.js";
+import FriendList from "./Component/FriendList.js";
+import LoginForm from "./Component/LoginForm.js";
+import Profile from "./Component/Profile.js";
+import FriendForm from "./Component/FriendForm.js";
+
+class MainContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      section: "main"
+    };
+  }
+
+  render() {
+    if (this.state.section === "main") {
+      return (
+        <div>
+          <p>Social Media Test Harness</p>
+          <LoginForm />
+          <PostForm />
+        </div>
+      );
+    } else if (this.state.section === "friends") {
+      return (
+        <div>
+          <p>Friends</p>
+          <FriendForm userid={sessionStorage.getItem("user")} />
+          <FriendList userid={sessionStorage.getItem("user")} />
+        </div>
+      );
+    } else if (this.state.section === "settings") {
+      return (
+        <div className="settings">
+          <p>Settings</p>
+          <Profile userid={sessionStorage.getItem("user")} />
+        </div>
+      );
+    } else {
+      return <p>Unidentified Section!</p>;
+    }
+  }
+}
+
+function setMenuOption(mode, maincontent, e) {
+  maincontent.current.setState({
+    section: mode
+  });
+}
+
+function App() {
+  let post = require("./post.svg");
+  let friend = require("./friends.svg");
+  let setting = require("./settings.svg");
+  let mainContent = React.createRef();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div id="sidenav" className="sidenav">
+          <ul id="side-menu-items">
+            <li className="pm admin student">
+              <button
+                className="link-button"
+                onClick={e => setMenuOption("main", mainContent, e)}
+              >
+                <img
+                  src={post}
+                  className="sidenav-icon"
+                  alt="Posts"
+                  title="Posts"
+                />
+              </button>
+            </li>
+            <li className="pm admin">
+              <button
+                className="link-button"
+                onClick={e => setMenuOption("friends", mainContent, e)}
+              >
+                <img
+                  src={friend}
+                  className="sidenav-icon"
+                  alt="Friends"
+                  title="Friends"
+                />
+              </button>
+            </li>
+            <li className="pm admin">
+              <button
+                className="link-button"
+                onClick={e => setMenuOption("settings", mainContent, e)}
+              >
+                <img
+                  src={setting}
+                  className="sidenav-icon"
+                  alt="Settings"
+                  title="Settings"
+                />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="maincontent" id="mainContent">
+          <MainContent ref={mainContent} />
+        </div>
+        <br />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Component/Profile.js
@@ -0,0 +1,115 @@
+import React from "react";
+import "../App.css";
+
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      firstname: "",
+      lastname: "",
+      responseMessage: ""
+    };
+    this.fieldChangeHandler.bind(this);
+  }
+
+  fieldChangeHandler(field, e) {
+    console.log("field change");
+    this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  componentDidMount() {
+    //make the api call to the user API to get the user with all of their attached preferences
+    fetch("http://stark.cse.buffalo.edu/hci/usercontroller.php", {
+      method: "post",
+      body: JSON.stringify({
+        action: "getCompleteUsers",
+        user_id: this.props.userid
+      })
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result.users) {
+            console.log(result.users);
+            this.setState({
+              // IMPORTANT!  You need to guard against any of these values being null.  If they are, it will
+              // try and make the form component uncontrolled, which plays havoc with react
+              username: result.users[0].username || "",
+              firstname: result.users[0].first_name || "",
+              lastname: result.users[0].last_name || ""
+            });
+          }
+        },
+        error => {
+          alert("error!");
+        }
+      );
+  }
+
+  submitHandler = event => {
+    //keep the form from actually submitting
+    event.preventDefault();
+
+    //make the api call to the user controller
+    fetch("http://stark.cse.buffalo.edu/hci/usercontroller.php", {
+      method: "post",
+      body: JSON.stringify({
+        action: "addOrEditUsers",
+        username: this.state.username,
+        firstname: this.state.firstname,
+        lastname: this.state.lastname,
+        user_id: sessionStorage.getItem("user"),
+        session_token: sessionStorage.getItem("token")
+      })
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            responseMessage: result.Status
+          });
+        },
+        error => {
+          alert("error!");
+        }
+      );
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.submitHandler} className="profileform">
+        <label>
+          Username
+          <input
+            type="text"
+            onChange={e => this.fieldChangeHandler("username", e)}
+            value={this.state.username}
+          />
+        </label>
+        <label>
+          First Name
+          <input
+            type="text"
+            onChange={e => this.fieldChangeHandler("firstname", e)}
+            value={this.state.firstname}
+          />
+        </label>
+        <label>
+          Last Name
+          <input
+            type="text"
+            onChange={e => this.fieldChangeHandler("lastname", e)}
+            value={this.state.lastname}
+          />
+        </label>
+        <input type="submit" value="submit" />
+        <p>Username is : {this.state.username}</p>
+        <p>Firstname is : {this.state.firstname}</p>
+        {this.state.responseMessage}
+      </form>
+    );
+  }
+}
