3af6768c7d49404538ce277644ab267aebbc04c8
Sean Brasse <seanbrasse@gmail.com>
2020-03-09 01:35:42 -0400

    Created User-Profile Markup + functional Settings Icon

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import AccountSettings from '../account-settings/AccountSettings';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import './Profile.css';
+import avatar from './img_avatar.png';
 
 export default function Profile() {
     let { userID } = useParams();
 
+    const followers = []
+    for (var i = 0; i <= 5; i++) {
+        followers.push(<button className="follower" key={i}>Follower<FontAwesomeIcon icon={faEllipsisV} className="btn-setting"></FontAwesomeIcon></button>)
+    }
+
     return (
         <main>
-            <h1>PROFILE {userID}</h1>
+            <div className="profile">
+                <img className="profile-image" src={avatar}></img>
+                <h1 className="profile-name">Profile {userID}</h1>
+                <Link to="/settings" className="settings"><FontAwesomeIcon icon={faCog} size="lg" onClick={AccountSettings} color="black"></FontAwesomeIcon></Link>
+            </div>
+            <div className="sidebar">
+                <h1 className="left-text">Bio</h1>
+                <p className="left-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h1 className="left-text">Followers</h1>
+                {followers}
+            </div>
         </main>
     );
 }
