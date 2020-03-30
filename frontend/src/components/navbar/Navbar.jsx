fa04544990f59ba6c7220cea1419c685cedd3298
seanbrasse <43007609+seanbrasse@users.noreply.github.com>
2020-03-30 14:41:51 -0400

    Seperated UserProfile from MyProfile

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from './Navbar.module.css';
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  var user_id = sessionStorage.getItem("userID");
+  var url = "/user/" + user_id;
+
   return (
     <nav className={styles.navbar}>
       <NavLink to="/feed">The Kitchen</NavLink>
       <ul>
         <li>
-          <NavLink to="/feed" activeClassName={styles.active}>Home</NavLink>
+          <NavLink to="/feed" activeClassName={styles.active}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/search" activeClassName={styles.active}>Search</NavLink>
+          <NavLink to="/search" activeClassName={styles.active}>
+            Search
+          </NavLink>
+        </li>
+        <li>
+          {" "}
+          {/* userLoggedIn ? */}
+          <NavLink to={url} activeClassName={styles.active}>
+            My Profile
+          </NavLink>
         </li>
       </ul>
-
-      {/* userLoggedIn ? <NavLink to="/user/{userid}">{ username }</NavLink> */}
     </nav>
   );
 }
--- frontend/src/pages/account-settings/AccountSettings.jsx
+++ frontend/src/pages/account-settings/AccountSettings.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./AccountSettings.css";
 
 export default function AccountSettings() {
-    return (
-        <main>
-            <h1>ACCOUNT SETTINGS</h1>
-        </main>
-    );
+  return (
+    <main>
+      <div className="TheBox">
+        <h1 className= "Header">ACCOUNT SETTINGS</h1>
+        <Link to="/forgot-password" className="ChangePassword">
+          {" "}
+          Change Your Password{" "}
+        </Link>
+        <br />
+        <Link to="" className="ChangeUsername">
+          {" "}
+          Change Your Username
+        </Link>
+      </div>
+    </main>
+  );
 }
