49b48257eb3170c45f9119f17ed4226d886e928e
Jonathan Romano <jaromano@buffalo.edu>
2020-04-20 15:31:27 -0400

    Initial pass on ingredient filter (#91)

--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -8,38 +8,51 @@ export default class Search extends React.Component {
         this.state = {
             posts: [],
             search: '',
+            excludeIngredients: '',
             fetchParams: {
                 action: 'getPosts',
                 posttype: 'Recipe',
-                posttext: ''
+                posttext: '',
+                tag_filters: []
             }
         }
     }
     
+    genTagFilters(state) {
+        return [
+            ...state.excludeIngredients.split(',').map(ing => ({include: false, tag: ing, type: 'ingredient'}))
+        ];
+    }
+
     render() {
         return (
             <main>
                 <h1>SEARCH</h1>
                 <form
-                    className={styles.searchbar}
-                    onSubmit={() => this.setState(prevState => ({fetchParams:{...prevState.fetchParams, posttext: prevState.search}}))}
+                    onSubmit={() => this.setState(prevState => ({fetchParams:{
+                        ...prevState.fetchParams,
+                        posttext: prevState.search,
+                        tag_filters: this.genTagFilters(prevState)
+                    }}))}
                 >
-                    <input
-                        type="text" value={this.state.search} className={styles.search}
-                        onChange={e => this.setState({search: e.target.value})}
-                    />
-                    <input
-                        type="submit" value="Search"
-                    />
+                    <div className={styles.searchbar}>
+                        <input
+                            type="text" value={this.state.search} className={styles.search}
+                            onChange={e => this.setState({search: e.target.value})}
+                        />
+                        <input
+                            type="submit" value="Search"
+                        />
+                    </div>
+                    <div className={styles.listWrapper}>
+                        <PostList fetchParams={this.state.fetchParams} className={styles.recipeList}/>
+                        <form className={`card ${styles.extraSearch}`}>
+                            <h3>Additional Search Details</h3>
+                            <h4>Exclude Ingredients</h4>
+                            <input type="text" onChange={e => this.setState({excludeIngredients: e.target.value})}/>
+                        </form>
+                    </div>
                 </form>
-                <div className={styles.listWrapper}>
-                    <PostList fetchParams={this.state.fetchParams} className={styles.recipeList}/>
-                    <form className={`card ${styles.extraSearch}`}>
-                        <h3>Additional Search Details</h3>
-                        <h4>Exclude Ingredients</h4>
-                        <input type="text"/>
-                    </form>
-                </div>
             </main>
         );
     }
